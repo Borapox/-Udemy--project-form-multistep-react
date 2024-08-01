bba3b8a48988da61f7c24e5d87bc90ce8f0d208f
Borapox <muller.mdi@gmail.com>
2024-08-01 16:39:51 -0300

    Criação da estrutura base do app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+import { GrFormNext, GrFormPrevious } from "react-icons/gr"
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+ <div>
+{/* Header */}
+  <div>
+    <h2>Deixe sua avaliação</h2>
+    <p>Ficamos felizes com a sua compra, utilize o formulário abaixo para avaliar o produto.</p>
+  </div>
+
+
+{/* Formulário */}
+  <div>
+    <p>Etapas</p>
+
+    <form>
+      <div></div>
+      <div>
+        <button type="button" className="border border-zinc-900">
+          <GrFormPrevious />
+          <span>Voltar</span>
+        </button>
+
+        <button type="submit" className="border border-zinc-900">
+          <GrFormNext />
+          Avançar
+        </button>
+      </div>
+    </form>
+  </div>
+
+ </div>
   )
 }
