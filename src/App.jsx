6687a8a6803dc1adb0fb0775e92e590fedc3d71f
Borapox <muller.mdi@gmail.com>
2024-08-02 18:49:10 -0300

    Lógica de avanço/retorno implementada aos botões. Criação do customHook 'useForm' para aplicar a lógica

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,60 @@
-import { GrFormNext, GrFormPrevious } from "react-icons/gr"
+//  Importações
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+import UserForm  from "./components/UserForm";
+import ReviewForm  from "./components/ReviewForm"
+import Thanks from "./components/Thanks"
 
-export default function App() {
+
+
+//  Sessão de Hooks
+import useForm from "./hooks/useForm"
+
+
+function App() { 
+  const formComponents = [<UserForm />, <ReviewForm></ReviewForm>,<Thanks></Thanks>];
+  const {currentStep, currentComponent, changeStep} = useForm(formComponents)
+  
   return (
  <div>
-{/* Header */}
-  <div>
-    <h2>Deixe sua avaliação</h2>
-    <p>Ficamos felizes com a sua compra, utilize o formulário abaixo para avaliar o produto.</p>
-  </div>
-
-
-{/* Formulário */}
-  <div>
-    <p>Etapas</p>
-
-    <form>
-      <div></div>
+      {/* Header */}
       <div>
-        <button type="button" className="border border-zinc-900">
-          <GrFormPrevious />
-          <span>Voltar</span>
-        </button>
-
-        <button type="submit" className="border border-zinc-900">
-          <GrFormNext />
-          Avançar
-        </button>
+        <h2>Deixe sua avaliação</h2>
+        <p>Ficamos felizes com a sua compra, utilize o formulário abaixo para avaliar o produto.</p>
       </div>
-    </form>
-  </div>
+
+
+      {/* Formulário */}
+      <div>
+        
+          <p>Etapas</p>
+
+          <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
+            <div>
+            {currentComponent}
+            </div>
+
+          <div className="flex gap-2">
+            <button
+              type="button" 
+              onClick={() => changeStep(currentStep - 1)}
+              className="border border-zinc-500 rounded-sm w-[120px] h-[30px] flex justify-center items-center bg-slate-200 hover:bg-slate-400">
+              <GrFormPrevious />
+              <span>Voltar</span>
+            </button>
+
+            <button 
+              type="submit" 
+              className="border border-zinc-500 rounded-sm w-[120px] h-[30px] flex justify-center items-center bg-slate-200 hover:bg-slate-400">
+              <GrFormNext />
+              <span>Avançar</span>
+            </button>
+          </div>
+          </form>
+
+      </div>
 
  </div>
   )
 }
+
+export default App
---
+++ src/components/UserForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const UserForm = () => {
+  return (
+    <div>
+        
+        <h2>UserForm</h2>
+
+    </div>
+  )
+}
+
+export default UserForm
